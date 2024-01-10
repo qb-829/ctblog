59660e4af2f559fb09ff11143b056dd41d0e76aa
qb-829 <publiqproperty@gmail.com>
2024-01-09 21:47:10 -0500

    update to login page

--- newblog/src/pages/Login.jsx
+++ newblog/src/pages/Login.jsx
@@ -33,7 +33,7 @@ const handleSubmit = async e => {
         <input required type="text" placeholder="username" name="username" onChange={handleChange}/>
         <input required type="password" placeholder="password" name="password" onChange={handleChange}/>
         <button onClick={handleSubmit}>LOGIN</button>
-        {err &&  <p>{err}</p>}
+        {err && <p>{err}</p>}
         <span>Don't have an account? <Link to="/register">Register</Link> </span>
       </form>
     </div>
